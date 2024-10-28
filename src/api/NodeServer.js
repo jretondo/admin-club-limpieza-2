6554c7c1da3b3a89cd761941d1c59ae380813981
Javier Retondo <jretondo90@gmail.com>
2024-10-28 10:18:08 -0300

    update

--- src/api/NodeServer.js
+++ src/api/NodeServer.js
@@ -16,8 +16,6 @@ if (process.env.NODE_ENV === "development") {
     publicFiles = "https://api-prod.nekoadmin.com.ar/club-limpieza/static"
 }
 
- host = "https://api-prod.nekoadmin.com.ar/club-limpieza/api"
-    publicFiles = "https://api-prod.nekoadmin.com.ar/club-limpieza/static"
 
 const prodImages = publicFiles + "/images/products/"
 
